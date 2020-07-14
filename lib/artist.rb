6753b8a755a25544da75959fdd662b3da276a99d
Teodoro Garcia III <tedtke514@gmail.com>
2020-07-14 19:44:05 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,8 @@
+class Artist 
+  attr_accessor :name 
+  
+  def artist_name
+    @name = name
+  end
+  
+end
---
+++ lib/author.rb
@@ -0,0 +1,9 @@
+class Author 
+  attr_accessor :name 
+  
+  def author_name
+    @name = name
+  end
+  
+  
+end
